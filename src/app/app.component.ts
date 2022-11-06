2b59fe607db41f96d21e6e7387095530bb9aef03
diego <diego.ochoa.maldonado@hotmail.com>
2022-11-05 18:02:34 -0600

    created landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EcommerceV1';
+
+  displayStyle = "none";
+  displayTicketTable = "none";
+
+  openTicketTableModal(){
+    this.displayTicketTable = "block";
+  }
+
+  closeTicketTableModal(){
+    this.displayTicketTable = "none";
+  }
+  
+  openPopup() {
+    this.displayStyle = "block";
+  }
+  closePopup() {
+    this.displayStyle = "none";
+  }
 }
