91d9ae6cca2c7edf6f3daa65aceb1db3248d2bac
d0mpsy <diego.ochoa.maldonado@hotmail.com>
2022-11-18 11:39:24 -0600

    udpated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { EventsComponent } from './Components/Events/events/events.component';
 
 const routes: Routes = [
 
-  { path: './', component: BodyComponent },
+  { path: './*', component: BodyComponent },
   { path: 'eventsComponent', component: EventsComponent },
   { path: 'eventsComponent/:id', component: EventsComponent },
   { path: 'bodyComponent', component: BodyComponent },
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
